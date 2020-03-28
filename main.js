53ff6a07e0ba82d9f32ae08400287f8ed1a6a006
rafeedm <rafeedmohtasim@gmail.com>
2020-03-28 03:07:21 -0400

    g sign in pt1

--- main.js
+++ main.js
@@ -70,3 +70,11 @@ var sw = {
 };
 
 window.addEventListener("load", sw.init);
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
